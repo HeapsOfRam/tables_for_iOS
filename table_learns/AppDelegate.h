55dfcf806ce42619b6702de9f0608c742de8cfe7
Ryan March <rmarch03@villanova.edu>
2014-11-18 11:25:26 -0500

    Initial Commit

---
+++ table_learns/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  table_learns
+//
+//  Created by Ryan March on 11/18/14.
+//  Copyright (c) 2014 Ryan March. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
